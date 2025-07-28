28d4a2ff1005a0ae7a3c21d55ceb437f1c6a03a1
Julya Jacometti <49757100803@>
2025-07-28 16:52:20 -0300

    Exercícios - args e kwargs. *Pendente

--- Aula 07 - Args e kwargs/Exercício01.py
+++ Aula 07 - Args e kwargs/Exercício01.py
@@ -1,1 +1,10 @@
 # Escreva uma função que receba um número variável de argumentos posicionais e retorne a soma de todos eles.
+
+def somar(*args):
+    # Criação da variável que acumulará a soma dos números
+    resultado = 0
+    # O looping for percorrerá cada valor passado como um argumento, iterando valor sobre valor
+    for num in args:
+        resultado += num # Atribui o valor de 'num' a variável resultado
+        return resultado
+
