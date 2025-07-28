9d322dd90a20f3e93ec8c29d99f1317e72feccb9
juujacometti <julyajaconetty@gmail.com>
2025-07-28 10:08:44 -0300

    Exercícios - Funções

--- Aula 06 - Funções/Funções com parâmetros/Desafio01.py
+++ Aula 06 - Funções/Funções com parâmetros/Desafio01.py
@@ -3,3 +3,28 @@ Criar uma função recursiva (que retorne ela mesma) para armazenar N termos da 
     * N é definido pelo usuário
 Ao encontrar os termos, imprimir a lista e finalizar a função.
 '''
+
+def fibonacci(n):
+    # Se N for 0 ou 1 retorna ele mesmo direto.
+    if n <= 1:
+        return n
+    # Se for maior, a função vai se chamar e calcular os dois números anteriores e somar eles.
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+lista_termos = []
+
+def gerar_lista(n, lista_termos, indice=0):
+    # Verificar se todos os termos já foram colocados na lista.
+    if indice == n:
+        print(lista_termos)
+        return
+
+    # Chama a função para calcular o próximo termo e armazena na lista.
+    lista_termos.append(fibonacci(indice))
+    # Chama ela mesma com N sendo igual, lista atualizada e o índice + 1 para ir para o próximo termo.
+    gerar_lista(n, lista_termos, indice + 1)
+
+n = int(input("Digite o número de termos da sequência Fibonacci que você deseja: "))
+
+gerar_lista(n, lista_termos)
--- Aula 06 - Funções/Funções com parâmetros/Desafio02.py
+++ Aula 06 - Funções/Funções com parâmetros/Desafio02.py
@@ -10,3 +10,122 @@ Atente-se às regras:
     - Só é possível alterar a senha se o usuário informar corretamente a senha atual.
     - Só é possível realizar logout se o usuário estiver logado.
 '''
+
+usuario = None
+senha = None
+logado = False
+
+def cadastrar():
+    global usuario, senha
+    print("===== CADASTRO =====\n")
+    if usuario is None:
+        usuario = str(input("Digite o nome de usuário: "))
+        senha = str(input(f"Digite a senha do {usuario}: "))
+        print(f"\nUsuário '{usuario}' cadastrado com sucesso!")
+    else:
+        print(f"\nUsuário '{usuario}' já cadastrado!")
+
+def login():
+    global usuario, senha, logado
+    if usuario is not None:
+        print("===== LOGIN =====\n")
+        usuario_digitado = str(input("Digite o nome do usuário: "))
+        if usuario_digitado.strip() == usuario: # Função strip() remove os espaços em branco do início e do fim
+            senha_digitada = str(input(f"Digite a senha de {usuario}: "))
+            if senha_digitada.strip() == senha:
+                print("\nLogin realizado com sucesso!")
+                logado = True
+            else:
+                print(f"\nSenha de {usuario} incorreta!")
+        else:
+            print(f"\n{usuario_digitado} não está cadastrado!")
+    else:
+        print("\nNão há nenhuma conta cadastrada no sistema!\n")
+
+def mudar_senha():
+    global usuario, senha, logado
+    print("===== ALTERAR SENHA =====\n")
+    if logado == True:
+        while True:
+            senha_digitada = str(input("Digite a senha atual: "))
+            if senha_digitada.strip() == senha:
+                senha_nova = str(input("Digite a nova senha: "))
+                senha = senha_nova
+                print("\nSenha alterada com sucesso!")
+                break
+            else:
+                print("\nSenha digitada está incorreta!\n")
+    else:
+        print("É preciso estar logado para alterar senha!")
+
+def logout():
+    global usuario, senha, logado
+    print("===== DESEJA SAIR? =====\n")
+    if logado == True:
+        while True:
+            confirmar = str(input("Tem certeza que deseja realizar logout? [SIM / NÃO]: "))
+            if confirmar.lower().strip() == "sim":
+                logado = False
+                print("\nEncerramento da conta realizado com sucesso!")
+                break
+            elif confirmar.lower().strip() == "não" or confirmar.lower().strip() == "nao":
+                print("\nCancelando logout...")
+                break
+            else:
+                print("\nDigite 'SIM' ou 'NÃO'!")
+    else:
+        print("\nÉ preciso estar logado para realizar saída de conta!")
+
+def excluir_conta():
+    global usuario, senha, logado
+    if logado == True:
+        print("===== DESEJA EXCLUIR CONTA? =====\n")
+        while True:
+            confirmar = str(input("Digite 'SIM' se tem certeza ou 'NÃO' para cancelar: "))
+            if confirmar.lower().strip() == 'sim':
+                usuario = None
+                senha = None
+                logado = False
+                print("\nExclusão de conta realizada com sucesso!")
+                break
+            elif confirmar.lower().strip() == 'não' or confirmar.lower().strip() == 'nao':
+                print("\nExclusão de conta cancelada com sucesso!")
+                break
+            else:
+                print("\nDigite 'SIM' ou 'NÃO'!")
+    else:
+        print("É preciso estar logado para realizar exclusão de conta!")
+
+def sair():
+    while True:
+        sair_sistema = str(input("\nTem certeza que deseja sair do sistema? [SIM / NÃO]: "))
+        if sair_sistema.lower().strip() == "nao" or sair_sistema.lower().strip() == "não":
+            print("\nCancelando saída do sistema...\n")
+            break
+        elif sair_sistema.lower().strip() == "sim":
+            print("\nSaindo...")
+            exit() # Finalizar sistema
+        else:
+            print("\nDigite 'SIM' ou 'NÃO'!")
+
+def menu():
+    while True:
+        print("==== MENU =====")
+        print("Seja bem vindo(a)!\n\nOpções Disponíveis:\n1 - Cadastro\n2 - Login\n3 - Alterar Senha\n4 - Sair da Conta\n5 - Excluir Conta\n6 - Sair do Sistema\n")
+        opcao = int(input("Digite o número da opção desejada: "))
+        if opcao == 1:
+            cadastrar()
+        elif opcao == 2:
+            login()
+        elif opcao == 3:
+            mudar_senha()
+        elif opcao == 4:
+            logout()
+        elif opcao == 5:
+            excluir_conta()
+        elif opcao == 6:
+            sair()
+        else:
+            print("\nDigite uma opção válida!\n")
+
+menu()
