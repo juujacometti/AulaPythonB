5bc57315b9add74218dfb8cd4680a1c2994bc223
juujacometti <julyajaconetty@gmail.com>
2025-07-29 10:34:38 -0300

    Exercícios - args e kwargs. *Pendente

--- Aula 07 - Args e kwargs/Exercício01.py
+++ Aula 07 - Args e kwargs/Exercício01.py
@@ -3,8 +3,23 @@
 def somar(*args):
     # Criação da variável que acumulará a soma dos números
     resultado = 0
+
     # O looping for percorrerá cada valor passado como um argumento, iterando valor sobre valor
     for num in args:
         resultado += num # Atribui o valor de 'num' a variável resultado
-        return resultado
+
+    return resultado
+
+lista = [] # Criação de lista para armazenar os valores
+
+print("Informe cada valor desejado para realizar a soma.\n * Caso deseje parar de inserir valores, digite 'N'.")
+
+while True: # Repete enquanto o usuário digita Strings numéricas
+    numero = input("Digite o valor para ser calculado: ")
+    if numero.isnumeric(): # Verifica se a String é numérica
+        lista.append(int(numero))
+    else:
+        break # Para quando encontrar uma String não numérica
+
+print(somar(*lista)) # Desempacota a lista e roda a função somar() com os seus argumentos
 
--- Aula 07 - Args e kwargs/Exercício02.py
+++ Aula 07 - Args e kwargs/Exercício02.py
@@ -1,1 +1,2 @@
 # Escreva uma função que receba um número variável de argumentos nomeados e retorne um dicionário com esses argumentos.
+
